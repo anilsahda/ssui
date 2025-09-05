a04a8b932b386e397f2ae6d1cf19fd8237a8d545
Anil <anilbca1996@gmail.com>
2025-09-05 18:05:52 +0530

    I have changes in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+export default nextConfig;
